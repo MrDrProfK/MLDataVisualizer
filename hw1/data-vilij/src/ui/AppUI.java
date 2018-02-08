a7feded572a2f4b607a672687bcbd9b3daa0a3ac
MrDrProfK <38258014+MrDrProfK@users.noreply.github.com>
2018-02-07 20:55:25 -0500

    Added necessary UI objects to scene and organized said objects using VBoxes nested inside an HBox (nested inside the root VBox).

--- hw1/data-vilij/src/ui/AppUI.java
+++ hw1/data-vilij/src/ui/AppUI.java
@@ -4,7 +4,10 @@ import actions.AppActions;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.ScatterChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import vilij.templates.ApplicationTemplate;
 import vilij.templates.UITemplate;
@@ -76,12 +79,38 @@ public final class AppUI extends UITemplate {
 
     private void layout() {
         // TODO for homework 1
+        // necessary axis declarations (with initial scale and range/domain params set)
         NumberAxis horizontalAxis = new NumberAxis(0, 110, 10);
         NumberAxis verticalAxis = new NumberAxis(0, 100, 10); 
-        chart = new ScatterChart<>(horizontalAxis,verticalAxis);
+        
+        // declare/initialize UI objects to be included in the first column
+        Label dataFileLabel = new Label("Data File");
         textArea = new TextArea();
         displayButton = new Button("Display");
-        appPane.getChildren().addAll(chart, textArea, displayButton);
+        
+        // create first column
+        VBox vbox0 = new VBox();
+        vbox0.setPrefWidth(windowWidth * .3);
+        // add elements to first column
+        vbox0.getChildren().addAll(dataFileLabel,textArea,displayButton);
+        
+        // declare/initialize UI objects to be included in the second column
+        Label dataVisLabel = new Label("Data Visualization");
+        chart = new ScatterChart<>(horizontalAxis,verticalAxis);
+        
+        // create second column
+        VBox vbox1 = new VBox();
+        vbox1.setPrefWidth(windowWidth * .65);
+        // add elements to second column
+        vbox1.getChildren().addAll(dataVisLabel, chart);
+        
+        // create a pane to hold both columns
+        HBox hbox = new HBox();
+        // add both columns (vbox0 and vbox1) to the HBox pane
+        hbox.getChildren().addAll(vbox0, vbox1);
+        
+        // add the pane containing both columns to inside of pre-existing VBox root pane
+        appPane.getChildren().add(hbox);
     }
 
     private void setWorkspaceActions() {
