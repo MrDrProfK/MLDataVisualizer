5b6d773ffbebd0f69de84f389a60adc8efa3783a
MrDrProfK <38258014+MrDrProfK@users.noreply.github.com>
2018-02-08 21:30:37 -0500

    Clicking the exit button now terminates the application. Added some of the necessary functionality for promptToSave().

--- hw1/data-vilij/src/actions/AppActions.java
+++ hw1/data-vilij/src/actions/AppActions.java
@@ -1,10 +1,19 @@
 package actions;
 
+import java.io.File;
+import java.io.FileWriter;
 import vilij.components.ActionComponent;
 import vilij.templates.ApplicationTemplate;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Optional;
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
+import javafx.stage.FileChooser;
 
 /**
  * This is the concrete implementation of the action handlers required by the application.
@@ -26,6 +35,12 @@ public final class AppActions implements ActionComponent {
     @Override
     public void handleNewRequest() {
         // TODO for homework 1
+        // try to prompt user to save current work before clearing and resetting data
+        try {
+            promptToSave();
+        } catch (IOException promptException) {
+            System.out.println(promptException.toString());
+        }
     }
 
     @Override
@@ -41,6 +56,9 @@ public final class AppActions implements ActionComponent {
     @Override
     public void handleExitRequest() {
         // TODO for homework 1
+        handleNewRequest(); // used for testing new button Functionality w/o activating new button
+        // terminate app execution when exit button is clicked
+        Platform.exit();
     }
 
     @Override
@@ -67,6 +85,50 @@ public final class AppActions implements ActionComponent {
     private boolean promptToSave() throws IOException {
         // TODO for homework 1
         // TODO remove the placeholder line below after you have implemented this method
-        return false;
+        // create and configure dialog box asking whether or not to save current work
+        Alert saveWorkDialogBox = new Alert(AlertType.CONFIRMATION);
+        saveWorkDialogBox.setTitle("Save Current Work");
+        saveWorkDialogBox.setHeaderText(null);
+        saveWorkDialogBox.setContentText("Would you like to save current work?");
+        
+        // create YES, NO, and CANCEL buttons for dialog box
+        ButtonType buttonTypeYes = new ButtonType("YES");
+        ButtonType buttonTypeNo = new ButtonType("NO");
+        ButtonType buttonTypeCancel = new ButtonType("CANCEL", ButtonData.CANCEL_CLOSE);
+
+        // add YES, NO, and CANCEL buttons to dialog box
+        saveWorkDialogBox.getButtonTypes().setAll(buttonTypeYes, buttonTypeNo, buttonTypeCancel);
+
+        // gather and analyze the dialog button clicked
+        Optional<ButtonType> result = saveWorkDialogBox.showAndWait();
+        if (result.get() == buttonTypeYes) {
+            FileChooser fileChooser = new FileChooser();
+            
+            // create and add FileChooser ExtensionFilter for Tab-Separated Data Files (*.tsd)
+            FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Tab-Separated Data File (*.tsd)", "*.tsd");
+            fileChooser.getExtensionFilters().add(extFilter);
+            
+            File file = fileChooser.showSaveDialog(applicationTemplate.getUIComponent().getPrimaryWindow());
+           
+            // create and write to new file if file is NOT null
+            if (file != null){
+                try {
+                    FileWriter fileWriter = null;
+
+                    fileWriter = new FileWriter(file);
+                    // write filler text to file for testing purposes
+                    fileWriter.write("filler");
+                    fileWriter.close();
+                } catch (IOException ex) {
+                    System.out.println(ex.toString());
+                }
+            }
+            
+        } else if(result.get() == buttonTypeCancel) {
+            // return false for CANCEL button click
+            return false;
+        }
+        // return true for both YES and NO button clicks
+        return true;
     }
 }
