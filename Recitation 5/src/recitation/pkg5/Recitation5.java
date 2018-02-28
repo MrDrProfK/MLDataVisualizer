b33673e003c1e5468477456c090a1c451f5dcc5a
MrDrProfK <38258014+MrDrProfK@users.noreply.github.com>
2018-02-28 12:05:41 -0500

    Added a third series to the chart that simply adds the Y-values of the first two. E.g., if the first series has a data point (1, 23) and the second has a point (1, 14), the third series' first point should be (1, 37). The application iterates over the points to sum their Y-values.

--- Recitation 5/src/recitation/pkg5/Recitation5.java
+++ Recitation 5/src/recitation/pkg5/Recitation5.java
@@ -1,5 +1,6 @@
 package recitation.pkg5;
 
+import java.util.Iterator;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
@@ -22,10 +23,10 @@ public class Recitation5 extends Application {
 
         lineChart.setTitle("Stock Monitoring, 2010");
         lineChart.setCreateSymbols(false);
-        
+
         XYChart.Series series = new XYChart.Series();
         series.setName("My portfolio");
-        
+
         series.getData().add(new XYChart.Data(1, 23));
         series.getData().add(new XYChart.Data(2, 14));
         series.getData().add(new XYChart.Data(3, 15));
@@ -38,10 +39,10 @@ public class Recitation5 extends Application {
         series.getData().add(new XYChart.Data(10, 17));
         series.getData().add(new XYChart.Data(11, 29));
         series.getData().add(new XYChart.Data(12, 25));
-        
+
         XYChart.Series series2 = new XYChart.Series();
         series2.setName("My portfolio 2");
-        
+
         series2.getData().add(new XYChart.Data(1, 25));
         series2.getData().add(new XYChart.Data(2, 16));
         series2.getData().add(new XYChart.Data(3, 17));
@@ -54,14 +55,35 @@ public class Recitation5 extends Application {
         series2.getData().add(new XYChart.Data(10, 11));
         series2.getData().add(new XYChart.Data(11, 20));
         series2.getData().add(new XYChart.Data(12, 29));
-        
+
+        XYChart.Series series3 = new XYChart.Series();
+        series3.setName("My portfolio 3");
+
+        // create iterators for each data set (each series)
+        Iterator seriesIterator = series.getData().iterator();
+        Iterator series2Iterator = series2.getData().iterator();
+
+        // while there is available chart data in both series...
+        while (seriesIterator.hasNext() && series2Iterator.hasNext()) {
+            // collect data object from series data
+            XYChart.Data s1Data = ((XYChart.Data) (seriesIterator.next()));
+            // extract x value from series data coordinate
+            Integer s1xVal = new Integer((s1Data.getXValue().toString()));
+            // extract y value from series data coordinate
+            Integer s1yVal = new Integer((s1Data.getYValue().toString()));
+            // extract y value from series2 data coordinate
+            Integer s2yVal = new Integer((((XYChart.Data) (series2Iterator.next())).getYValue().toString()));
+            // add corresponding y-values (from series and series2) to form series3
+            series3.getData().add(new XYChart.Data(s1xVal, s1yVal + s2yVal));
+        }
         Scene scene = new Scene(lineChart, 800, 600);
-        lineChart.getData().addAll(series, series2);
+        // add data from series, series2, and series3 to the line chart
+        lineChart.getData().addAll(series, series2, series3);
 
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-    
+
     public static void main(String[] args) {
         launch(args);
     }
