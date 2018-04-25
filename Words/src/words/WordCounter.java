85bb166b02698a2159df87cb249a1112756c1b4a
MrDrProfK <38258014+MrDrProfK@users.noreply.github.com>
2018-04-25 12:09:19 -0400

    Optimized CPU usage.

--- Words/src/words/WordCounter.java
+++ Words/src/words/WordCounter.java
@@ -1,28 +1,33 @@
 package words;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
 public class WordCounter {
     private final List<CountedWord> words;
+    private final HashMap<Word, CountedWord> wordsForFinding;
 
     WordCounter() {
 	words = new LinkedList<>();
+        wordsForFinding = new HashMap<>();
     }
     
     private CountedWord findWord(Word w) {
-        for(int i = 0; i < words.size(); i++) {
-            CountedWord cw = words.get(i);
-            if(cw.getWord().equals(w))
-                return cw;
-        }
-        return null;
+        return wordsForFinding.get(w);
+//        for(int i = 0; i < words.size(); i++) {
+//            CountedWord cw = words.get(i);
+//            if(cw.getWord().equals(w))
+//                return cw;
+//        }
+//        return null;
     }
 
     void countWord(Word w) {
         CountedWord cw = findWord(w);
 	if(cw == null) {
 	    cw = new CountedWord(w);
+            wordsForFinding.put(w, cw);
             words.add(cw);
         }
 	cw.tally();
