7b42f4e69c7b2a0e3e47567694609aade7ff2a3d
MrDrProfK <38258014+MrDrProfK@users.noreply.github.com>
2018-04-27 23:55:07 -0400

    Modified algorithm to have knowledge of the chart.

--- hw1/data-vilij/src/classification/RandomClassifier.java
+++ hw1/data-vilij/src/classification/RandomClassifier.java
@@ -8,6 +8,12 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicBoolean;
+import javafx.application.Platform;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.ValueAxis;
+import javafx.scene.chart.XYChart;
+import ui.AppUI;
 
 /**
  * @author Ritwik Banerjee
@@ -25,6 +31,8 @@ public class RandomClassifier extends Classifier {
 
     // currently, this value does not change after instantiation
     private final AtomicBoolean tocontinue;
+    
+    private final LineChart<Number, Number> chart;
 
     @Override
     public int getMaxIterations() {
@@ -44,11 +52,12 @@ public class RandomClassifier extends Classifier {
     public RandomClassifier(DataSet dataset,
                             int maxIterations,
                             int updateInterval,
-                            boolean tocontinue) {
+                            boolean tocontinue, LineChart<Number, Number> chart) {
         this.dataset = dataset;
         this.maxIterations = maxIterations;
         this.updateInterval = updateInterval;
         this.tocontinue = new AtomicBoolean(tocontinue);
+        this.chart = chart;
     }
 
     @Override
@@ -64,6 +73,8 @@ public class RandomClassifier extends Classifier {
             // everything below is just for internal viewing of how the output is changing
             // in the final project, such changes will be dynamically visible in the UI
             if (i % updateInterval == 0) {
+
+
                 System.out.printf("Iteration number %d: ", i); //
                 flush();
             }
@@ -78,12 +89,44 @@ public class RandomClassifier extends Classifier {
     // for internal viewing only
     protected void flush() {
         System.out.printf("%d\t%d\t%d%n", output.get(0), output.get(1), output.get(2));
+
+        //defining a series
+        XYChart.Series series = new XYChart.Series();
+        series.setName("Random Classifier");
+        //populating the series with data
+        series.getData().add(new XYChart.Data(1, 5));
+        series.getData().add(new XYChart.Data(2, 7));
+        
+        Platform.runLater(() -> {
+
+//                double xMin = ((ValueAxis) chart.getXAxis()).getLowerBound();
+//                double xMax = ((ValueAxis) chart.getXAxis()).getUpperBound();
+//                
+//                double yMin = ((ValueAxis) chart.getYAxis()).getLowerBound();
+//                double yMax = ((ValueAxis) chart.getYAxis()).getUpperBound();
+//                
+//                double tempX, tempY;
+//                tempY = ((-xCoefficient * xMin) - constant) / yCoefficient;
+//                if (tempY > yMax) {
+//
+//                }
+//                double pt1x, pt1y;
+//                pt1y = 
+//                double pt2x, pt2y;
+                
+//                series.getData().forEach(d -> {
+//                    ((XYChart.Data)d).getNode().setVisible(false);
+//                });
+//                series.getNode().lookup(".chart-series-area-line").setStyle("-fx-stroke: rgba(" + rgb + ", 1.0);");;
+//chart.getData().remove
+chart.getData().add(series);
+        });
     }
 
     /** A placeholder main method to just make sure this code runs smoothly */
     public static void main(String... args) throws IOException {
         DataSet          dataset    = DataSet.fromTSDFile(Paths.get("/path/to/some-data.tsd"));
-        RandomClassifier classifier = new RandomClassifier(dataset, 100, 5, true);
-        classifier.run(); // no multithreading yet
+//        RandomClassifier classifier = new RandomClassifier(dataset, 100, 5, true, new LineChart());
+//        classifier.run(); // no multithreading yet
     }
 }
